bedb0da944e758f381afe04901e9c43e4738de18
Nabila Taufiq <nabila.taufiq@verizon.com>
2019-06-21 12:38:37 -0500

    Update
    Added comments.

--- js/script.js
+++ js/script.js
@@ -3,19 +3,9 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+   I added two global variables, one to store all the students in the list,
+   and the other to store the number of students that should be on each page.
 ***/
 
 const listItem =  document.querySelectorAll('.student-item');
@@ -23,22 +13,17 @@ const numberOfItems = 10;
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+/***
+1. I created a showPage function to create pages.  I did so by hiding all the other students 
+   except for the ones that I want to be shown on that page.
+2. I created two variables to store the index numbers of the first student and the last student.
+3. I used the for loop to loop through the list. 
+   - The if statement is used to show that if the index number of the student is between the index 
+     number of the first student and the last student, then to show it, otherwise hide it.
 ***/
 
 const showPage = (listItem, page) => {
+
    let startIndex = (page*numberOfItems)-numberOfItems;
    let endIndex = page*numberOfItems;
 
@@ -49,29 +34,47 @@ const showPage = (listItem, page) => {
       } else {
          listItem[i].style.display = 'none';
       }
-
    }
-
 }
 
 
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+I created a appendPageLinks function to create buttons, that when clicked will show the appropriate page (or list of students).
+
+1. I created my variables:
+   - divElements to create and store a div.  
+   - pageClass to select and store div with the class name page.  
+   - ulElement to create and store ul.
+   - numberOfPages to store number of pages I will need.  I did so by dividing the total number of students by the number I want to be shown on each page.  Then I rounded it up.
+2. I added a class name 'pagination' to the div element that I just created.
+3. Then I appended the div that I just created to the .page div.
+4. I appended the ul that I just created to the div with class name 'pagination'.
+5. I used a for loop.  
+   a. Inside the for loop I created two variables.
+      - I created a li tag and stored it inside the variable liElement.
+      - I created an a tag, and stored it inside the variable aTag.
+   b. Then I appended the liElement to the UlElement. 
+   c. I appended the a tag inside the li tag.  
+   d. I created an attribute inside the a tag, named "href", and i set the value equal to "#".
+   e. I changed the inner html of the a tag to show the number of the page.  By doing this step, it now shows the number of the page on the button.
+   f. Then I selected the first link, and set the class name to active, so that when the page is loaded, and the first page is shown, the page 1 button is highlighted.
+   f. Then I added the functionalities of the buttons by adding an event listener.  So that when the button it clicked, it takes to the page.
+      - Inside the event listener, i added the behavior 'click', so when I click, event happens.
+      - I have added two variables.  I selected all the a tags, and stored it inside the variable page.  and I selected the text content of the button I am clicking, and storing it inside the variable button.
+      - then I called the showPage function, so that when I click the button, the appropriate list item is shown.
+      - I added a for loop to loop through the a tags, which are my buttons, and remove the class named "active".  So that all the other button except for the one that is clicked appreas unselected.
+      - Outside the for loop and inside the event listener, I set the class name of the button I am clicking to 'active', so that that button is highlighted.
 ***/
-
-
 const appendPageLinks = (listItem) => {
    let divElement = document.createElement('div');
    let pageClass = document.querySelector('.page');
    let ulElement = document.createElement('ul');
-    
-   //divElement.setAttribute('class', 'pagination');
+   let numberOfPages = Math.ceil(listItem.length/numberOfItems);
+   
    divElement.className = 'pagination';
    pageClass.appendChild(divElement);
    divElement.appendChild(ulElement);
-   let numberOfPages = Math.ceil(listItem.length/numberOfItems);
 
    for (let i = 1; i<=numberOfPages; i++){
       let liElement = document.createElement('li');
@@ -79,48 +82,30 @@ const appendPageLinks = (listItem) => {
       ulElement.appendChild(liElement);
       liElement.appendChild(aTag).setAttribute("href", '#');
       aTag.innerHTML = i;
+      document.querySelector('.pagination a:first-child').className = "active";
+
+      
 
       aTag.addEventListener('click',(event)=>{
          let page = document.querySelectorAll('li a');
          let button = event.target.textContent;
          showPage(listItem, button);
 
+         for(i=0; i<page.length; i++){
+            page[i].classList.remove("active");
+         }
+   
          event.target.className = "active";
-
-         for(i=0, i<)
-      })
-
-      
+      });
 
    }
-
-
-  
-
-   // Next up we call the slice function of our Array and tell it where to start slicing from and where to 
-   // end. The result will be an Array with just the elements that we're currently looking for. The 
-   // algorithm is pretty self-explanatory. You start at the current page, minus one, plus the page 
-   // offset. And the end is just that start number plus the offset again. The 2 new functions at the
-   //  bottom drawList and check will render the sublist and then update the pager buttons respectively.
-
-
-
-   //let ulElement = document.getElementsByClassName('pagination');
-   //ulElement.createElement('ul');
-   // A nested UL element containing one LI element for every ten students in the list.
-   //document.createElement('ul');
-
-   //let numberOfPages = Math.Ceiling(listItem.length/10);
-   //for (i=0, i<numberOfPages.length)
-
 }
 
+/***
+ I called the showPage function, so that when the page is loaded, it shows the first page.
+ I called the appendPageLinks function so that it shows all the buttons.
 
+***/
 
 showPage(listItem, 1);
 appendPageLinks(listItem);
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
